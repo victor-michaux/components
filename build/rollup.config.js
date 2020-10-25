fd971d4608e16b5417e75f425607496a1d7cbf1f
Victor Michaux <victor.michaux@gmail.com>
2020-10-26 00:38:22 +0100

    Add esm

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,28 +2,55 @@ import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to E
 import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
 import postcss from 'rollup-plugin-postcss'
 
-export default {
-    input: './main.js', // Path relative to package.json
-    external: [
-        'vue'
-    ],
-    output: {
-        file: 'dist/components.umd.js',
-        format: 'umd',
-        name: 'Components',
-        exports: 'named',
+export default [
+    {
+        input: './main.js', // Path relative to package.json
+        external: [
+            'vue'
+        ],
+        output: {
+            file: 'dist/components.umd.js',
+            format: 'umd',
+            name: 'Components',
+            exports: 'named',
+        },
+        plugins: [
+            commonjs(),
+            vue({
+                css: true, // Dynamically inject css as a <style> tag
+                compileTemplate: true, // Explicitly convert template to render function
+            }),
+            postcss({
+                // extract: true,
+                config: {
+                    path: './postcss.config.js',
+                }
+            }),
+        ],
     },
-    plugins: [
-        commonjs(),
-        vue({
-            css: true, // Dynamically inject css as a <style> tag
-            compileTemplate: true, // Explicitly convert template to render function
-        }),
-        postcss({
-            // extract: true,
-            config: {
-                path: './postcss.config.js',
-            }
-        }),
-    ],
-};
+    {
+        input: './main.js', // Path relative to package.json
+        external: [
+            'vue'
+        ],
+        output: {
+            file: 'dist/components.es.js',
+            format: 'esm',
+            name: 'Components',
+            exports: 'named',
+        },
+        plugins: [
+            commonjs(),
+            vue({
+                css: true, // Dynamically inject css as a <style> tag
+                compileTemplate: true, // Explicitly convert template to render function
+            }),
+            postcss({
+                // extract: true,
+                config: {
+                    path: './postcss.config.js',
+                }
+            }),
+        ],
+    }
+];
